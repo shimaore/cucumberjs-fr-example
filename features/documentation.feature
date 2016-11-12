20734ac76778857aba19a40d7ab37b90715b6158
Stephane Alnet <stephane@shimaore.net>
2016-11-12 14:48:10 +0100

    initial

---
+++ features/documentation.feature
@@ -0,0 +1,11 @@
+# language: fr
+# features/documentation.feature
+Fonctionnalité: Documentation
+  En tant qu'utilisateur de Cucumber.js
+  Je veux avoir de la documentation sur Cucumber
+  Afin de pouvoir me concentrer sur la constructions d'applications géniales
+
+  Scénario: Lire la documentation
+    Étant donné que je suis sur le repo Github de Cucumber.js
+    Quand je clique sur "CLI"
+    Alors je devrais voir "Running specific features"
---
+++ features/step_definitions/browser_steps.js
@@ -0,0 +1,21 @@
+// features/step_definitions/browser_steps.js
+var seleniumWebdriver = require('selenium-webdriver');
+
+module.exports = function () {
+  this.Given(/^je suis sur le repo Github de Cucumber\.js$/, function() {
+    console.log(this.driver)
+    return this.driver.get('https://github.com/cucumber/cucumber-js/tree/master');
+  });
+
+  this.When(/^je clique sur "([^"]*)"$/, function (text) {
+    return this.driver.findElement({linkText: text}).then(function(element) {
+      return element.click();
+    });
+  });
+
+  this.Then(/^je devrais voir "([^"]*)"$/, function (text) {
+    var xpath = "//*[contains(text(),'" + text + "')]";
+    var condition = seleniumWebdriver.until.elementLocated({xpath: xpath});
+    return this.driver.wait(condition, 5000);
+  });
+};
